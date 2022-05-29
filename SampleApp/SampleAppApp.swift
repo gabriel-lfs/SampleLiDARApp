e1b7bbf6ceafa43ce92894a9a358cee6aa1d2102
Gabriel Luís Fernando vieira de Souza <gabriel-soz@live.com>
2022-05-29 16:38:33 -0300

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Gabriel Souza on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
